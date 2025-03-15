d41e35e172f006d69883b5dc957af96c7539a5cd
IsHPDuwu <120182813+IsHPDuwu@users.noreply.github.com>
2025-03-16 00:54:29 +0800

    feat: 允许添加多个同一小组件

--- menu.py
+++ menu.py
@@ -1126,8 +1126,8 @@ class SettingsMenu(FluentWindow):
     def ct_add_widget(self):
         widgets_list = self.findChild(ListWidget, 'widgets_list')
         widgets_combo = self.findChild(ComboBox, 'widgets_combo')
-        if not widgets_list.findItems(widgets_combo.currentText(), QtCore.Qt.MatchFlag.MatchExactly):
-            widgets_list.addItem(widgets_combo.currentText())
+        # if not widgets_list.findItems(widgets_combo.currentText(), QtCore.Qt.MatchFlag.MatchExactly):
+        widgets_list.addItem(widgets_combo.currentText())
         self.ct_update_preview()
 
     def ct_remove_widget(self):
